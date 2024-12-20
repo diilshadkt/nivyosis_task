bdaaad901f460734a0f07d8ec5acfa0056d22423
diilshadkt <dilshadktkunnathodi@gmail.com>
2024-12-20 09:10:15 +0530

    delete alert dialog ui added

--- lib/features/home/view/widgets/users_table_widget.dart
+++ lib/features/home/view/widgets/users_table_widget.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:nivyosis_task/features/home/controller/user_controller.dart';
 import 'package:nivyosis_task/features/home/model/get_user_model.dart';
 
 class UsersTableWidget extends ConsumerWidget {
-  const UsersTableWidget({super.key});
+  
+  const UsersTableWidget({super.key,});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    void showDeleteDialog(
+      BuildContext context,
+    ) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: const Text("Are you sure you want to delete this item?"),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  context.pop();
+                },
+                child: const Text("Cancel"),
+              ),
+              TextButton(
+                onPressed: () async {
+                  // Navigator.of(context).pop();
+                  // await _deleteProduct(context, id, dataBox);
+                },
+                child: const Text("Delete"),
+              ),
+            ],
+          );
+        },
+      );
+    }
+
     return Padding(
       padding: EdgeInsets.only(left: MediaQuery.sizeOf(context).width / 12),
       child: Column(
@@ -67,7 +97,8 @@ class UsersTableWidget extends ConsumerWidget {
                             IconButton(
                                 onPressed: () {}, icon: Icon(Icons.visibility)),
                             IconButton(
-                                onPressed: () {}, icon: Icon(Icons.delete))
+                                onPressed: () => showDeleteDialog(context),
+                                icon: Icon(Icons.delete))
                           ],
                         )
                       ],
